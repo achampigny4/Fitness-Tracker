98e9f199ccffc4f3904bd7cc374fbe34e15b3134
Alissa Champigny <achampigny4@gmail.com>
2020-10-13 14:53:32 -0400

    start of workouts router

---
+++ routes/workouts.js
@@ -0,0 +1,81 @@
+// projects 2 router  review it
+
+const express = require("express");
+const db = require("../models");
+
+//require in index.js router
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const workouts = await db.Workout.find({});
+        res.json(workouts);
+        // res.get(workouts);
+    } catch (err) {
+        res.status(500).statusMessage(err).end();
+    }
+});
+
+// router.get("", async () => {
+
+// });
+
+// router.post("", async () => {
+
+// });
+
+// router.put("", async () => {
+
+// });
+
+// router.delete("", async () => {
+
+// });
+//export to index.js
+module.exports = router;
+
+// index.js to export models to server?
+// const db = require("./models");
+
+
+// app.get("/notes", (req, res) => {
+//   db.Note.find({})
+//     .then(dbNote => {
+//       res.json(dbNote);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/user", (req, res) => {
+//   db.User.find({})
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.post("/submit", ({ body }, res) => {
+//   db.Note.create(body)
+//     .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/populateduser", (req, res) => {
+//   db.User.find({})
+//     .populate("notes")
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
