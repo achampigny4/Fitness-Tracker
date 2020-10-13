ceb55de69608d6313b0811201c4f97df6b0f7907
Alissa Champigny <achampigny4@gmail.com>
2020-10-13 15:56:48 -0400

    file path update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,17 +5,16 @@ const router = express.Router();
 const path = require("path");
 
 //routes to html files go here
-//public not needed because server?
 router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "/exercice.html"))
+    res.sendFile(path.join(__dirname, "../public/exercice.html"))
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "/stats.html"))
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 module.exports = router;
